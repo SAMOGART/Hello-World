5f550c8c60ac73ebdf0f2daa3d2cbeb9819fba14
ARTEMII <89671480088@mail.ru>
2021-11-18 14:37:27 +0300

    kill switch is created. Yeeaaa

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label1: UILabel!
+    
+    
+    @IBOutlet var buttonka1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label1.isHidden = true
+        
+        buttonka1.layer.cornerRadius = 10
+        
     }
 
-
+    @IBAction func buttonAction1() {
+        label1.isHidden.toggle()
+        
+        if label1.isHidden {
+        buttonka1.setTitle("Show label", for: .normal)
+        } else {
+        buttonka1.setTitle("Show nothing", for: .normal)
+        }
+        
+    }
+    
 }
 
